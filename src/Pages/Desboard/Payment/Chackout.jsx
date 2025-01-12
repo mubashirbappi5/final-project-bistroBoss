b641c286c344f118087f1e62d9213461fd1617bb
mubashirbappi <mubashirbappi@gmail.com>
2025-01-12 14:29:19 +0600

    payment sucess

--- src/Pages/Desboard/Payment/Chackout.jsx
+++ src/Pages/Desboard/Payment/Chackout.jsx
@@ -1,10 +1,27 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import useAxios from '../../../Hooks/useAxios';
+import useCarts from '../../../Hooks/useCarts';
+import useAuth from './../../../Hooks/useAuth';
 
 const Chackout = () => {
     const [error,seterror] = useState('')
+
+    const [clientSecret,setclientSecret]=useState('')
     const stripe = useStripe();
     const elements = useElements();
+    const axiosscure = useAxios();
+    const [cart] = useCarts()
+    const {user}=useAuth()
+    const price = cart.reduce((total,item)=>total+item.price,0)
+    useEffect(()=>{
+    axiosscure.post('create-payment-intent',{price:price})
+    .then(res=>{
+        console.log(res.data.clientSecret)
+        setclientSecret(res.data.clientSecret)
+    })
+
+    },[cart,price])
     const handleSubmit = async(e)=>{
         e.preventDefault();
 
@@ -28,12 +45,32 @@ const Chackout = () => {
             console.log('[PaymentMethod]', paymentMethod);
             seterror(null)
           }
+        //   confim pay
+        const {paymentIntent,error:confimerror} = await stripe.confirmCardPayment(clientSecret,{
+            payment_method:{
+                card:card,
+                billing_details:{
+                    name:user?.displayName || 'anonymous',
+                    email:user?.email || 'anonymous',
+                }
+            }
+        })
+        if(confimerror){
+            console.log('confim error:',confimerror)
+        }
+        else{
+            console.log(paymentIntent)
+            if(paymentIntent.status==='succeeded'){
+                alert('payment succesful!')
+            }
+        }
        
       
 
     }
     return (
         <form className='flex flex-col  ' onSubmit={handleSubmit}>
+            <h1 className='font-semibold text-center my-10'>Total Pay Amount: {price.toFixed(2)}</h1>
             <CardElement
             options={{
                 style: {
@@ -51,7 +88,7 @@ const Chackout = () => {
               }}
             >
                   </CardElement>
-                <button className='btn btn-wide bg-orange-300 mt-5 ' type="submit" disabled={!stripe}>
+                <button className='btn btn-wide bg-orange-300 mt-5 ' type="submit" disabled={!stripe ||!clientSecret }>
         Pay 
       </button>
       <p className='text-red-500'>{error}</p>
